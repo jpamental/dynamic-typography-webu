64276fff34d0dec44c7b48749186129b80416345
Jason Pamental <jason@rwt.io>
2019-09-11 18:19:05 -0400

    Adding Google Fonts VFs

--- js/main.js
+++ js/main.js
@@ -140,3 +140,82 @@ $(function () {
         $pane.delay(100).animate({scrollLeft: new_left});
     });
 });
+
+
+function variableResize() {
+
+  let root = document.documentElement;
+  let bodyStyles = window.getComputedStyle(document.body);	
+		
+	// Minimum and Maximum viewport size - must be unitless em values
+	const maxWindowSize = bodyStyles.getPropertyValue('--bp-xlarge') * 16;
+	const minWindowSize = bodyStyles.getPropertyValue('--bp-small') * 16;
+	
+	// Get current viewport size
+	const windowWidth = window.innerWidth
+
+  // Scale elements and min/max values
+  const pWeightVar = '--p-wght';
+	const pMinFontWeight = bodyStyles.getPropertyValue('--p-wght-min');
+	const pMaxFontWeight = bodyStyles.getPropertyValue('--p-wght-max');
+	scale(pWeightVar, pMinFontWeight, pMaxFontWeight);
+
+  const h1WeightVar = '--h1-wght';
+	const h1MinFontWeight = bodyStyles.getPropertyValue('--h1-wght-min');
+	const h1MaxFontWeight = bodyStyles.getPropertyValue('--h1-wght-max');
+	scale(h1WeightVar, h1MinFontWeight, h1MaxFontWeight);
+
+	const h1XPRNVar = '--h1-XPRN';
+	const h1MinFontXPRN = bodyStyles.getPropertyValue('--h1-XPRN-min');
+	const h1MaxFontXPRN = bodyStyles.getPropertyValue('--h1-XPRN-max');
+	scale(h1XPRNVar, h1MinFontXPRN, h1MaxFontXPRN);
+
+	const h1SlntVar = '--h1-slnt';
+	const h1MinFontSlnt = bodyStyles.getPropertyValue('--h1-slnt-min');
+	const h1MaxFontSlnt = bodyStyles.getPropertyValue('--h1-slnt-max');
+	scale(h1SlntVar, h1MinFontSlnt, h1MaxFontSlnt);
+
+	const h2WeightVar = '--h2-wght';
+	const h2MinFontWeight = bodyStyles.getPropertyValue('--h2-wght-min');
+	const h2MaxFontWeight = bodyStyles.getPropertyValue('--h2-wght-max');
+	scale(h2WeightVar, h2MinFontWeight, h2MaxFontWeight);
+
+	const h2XPRNVar = '--h2-XPRN';
+	const h2MinFontXPRN = bodyStyles.getPropertyValue('--h2-XPRN-min');
+	const h2MaxFontXPRN = bodyStyles.getPropertyValue('--h2-XPRN-max');
+	scale(h2XPRNVar, h2MinFontXPRN, h2MaxFontXPRN);
+
+	const h2SlntVar = '--h2-slnt';
+	const h2MinFontSlnt = bodyStyles.getPropertyValue('--h2-slnt-min');
+	const h2MaxFontSlnt = bodyStyles.getPropertyValue('--h2-slnt-max');
+	scale(h2SlntVar, h2MinFontSlnt, h2MaxFontSlnt);
+	
+	function scale(varName, minValue, maxValue) {
+		// Make sure min/max are numbers
+		minValue = minValue * 1;
+		maxValue = maxValue * 1;
+
+		//Scale within a range
+		const percent = (windowWidth - minWindowSize) / (maxWindowSize - minWindowSize);
+		if (maxValue < minValue) {
+			var valueScale = minValue - (percent * (minValue - maxValue));
+		} else {
+			var valueScale = (percent * (maxValue - minValue)) + minValue;
+		}
+	  
+		// Get the new font width
+		const newValue = windowWidth > maxWindowSize
+		   ? maxValue 
+		   : windowWidth < minWindowSize 
+				? minValue 
+				: valueScale;
+	   
+			// Set my CSS Custom Property for width to update.
+		root.style.setProperty(varName, newValue);
+	}
+
+
+}
+
+window.addEventListener("load", variableResize);
+window.addEventListener("resize", variableResize);
